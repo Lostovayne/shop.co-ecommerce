132df86f653ff5478d7214bbff240f164b32d759
LostoVayne <moises.barrera.devmail.com>
2024-03-14 15:40:34 -0300

    agregado el navbar desktop

---
+++ app/(public)/_components/offer-bar.tsx
@@ -0,0 +1,9 @@
+export const OfferBar = () => {
+  return (
+    <div className="bg-black px-5 py-2 h-10">
+      <p className="font-normal text-center text-sm text-white">
+        Sign up and get 20% off to your first order. <span className="underline leading-5 tracking-wide" >Sign Up Now</span>
+      </p>
+    </div>
+  );
+};
