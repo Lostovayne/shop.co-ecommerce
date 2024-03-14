ea21df45b297f2e2fd982899379b3cc70ab54150
CVazquezOpalka <cristianvazquez80@gmail.com>
2024-03-14 19:51:52 -0300

    add newsLetter in the footer section

--- app/(public)/_components/footer.tsx
+++ app/(public)/_components/footer.tsx
@@ -1,5 +1,11 @@
+import Newsletter from "./newsletter";
+
 export const Footer = () => {
   return (
-    <div className="bg-blue-200 h-96 xl:h-64" >Footerxxxxxxxxxxxxxxxxx</div>
-  )
-}
+    <>
+      <div className="bg-blue-200 h-96 xl:h-64">
+      
+      </div>
+    </>
+  );
+};
---
+++ app/(public)/_components/newsletter.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Newsletter() {
+  return (
+    <div className="flex items-center bg-black justify-between  xl:max-w-screen-xl max-2xl:max-w-screen-lg rounded-md md:h-[180px] absolute top-[-90px] left-[20%] right-[20%]">
+      <div className="flex items-center w-[50%] flex-col  ">
+        <h1 className=""></h1>
+      </div>
+      <div className="flex items-center  w-[50%] flex-col "></div>
+    </div>
+  );
+}
+
+export default Newsletter;
