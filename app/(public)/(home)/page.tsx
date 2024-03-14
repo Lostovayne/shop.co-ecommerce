482fd3b844dd8a6fc38e17ceec22b588c0677387
LostoVayne <moises.barrera.devmail.com>
2024-03-14 20:11:57 -0300

    agregados los elementos del footer

--- app/(public)/(home)/page.tsx
+++ app/(public)/(home)/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "../_components/hero-section";
 
 const HomePage = () => {
   return (
-    <div className="bg-red-50 min-h-screen">
+    <div className="bg-red-50 px-4 min-h-screen">
       <HeroSection />
     </div>
   );
