dcf72115c21c4de38388300a69a1c1723bb07f5b
LostoVayne <moises.barrera.devmail.com>
2024-03-15 18:32:16 -0300

    agregados los cambios en el footer y en el newsletter

--- app/(public)/_components/newsletter.tsx
+++ app/(public)/_components/newsletter.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import { GoMail } from "react-icons/go";
 import { Button } from "@/components/ui/button";
+import { RiMailLine } from "react-icons/ri";
 
 function Newsletter() {
   return (
-    <div className="flex flex-col md:flex-row justify-around items-center bg-black md:justify-between h-[293px] md:h-[180px] xl:max-w-screen-xl max-2xl:max-w-screen-lg rounded-xl absolute top-[-90px] left-[20%] right-[20%]">
-      <div className="flex items-center w-[80%] md:w-[60%] flex-col">
-        <h1 className="text-3xl text-center w-full md:text-4xl font-extrabold text-white uppercase md:w-[80%] md:pl-4 md:text-start ">
-          Stay up to date about our latest offers
+    <div className="top-[-90px] right-[0.9rem] left-[0.9rem] absolute flex md:flex-row flex-col justify-evenly md:justify-between items-center bg-black 2xl:mx-auto rounded-xl max-2xl:max-w-screen-lg xl:max-w-screen-xl h-[285px] md:h-[180px]">
+      <div className="flex flex-col items-center w-[80%] md:w-[50%]">
+        <h1 className="w-full md:w-[80%] font-extrabold text-3xl text-pretty text-white md:text-4xl md:text-start uppercase">
+          Stay upto date about our latest offers
         </h1>
       </div>
-      <div className="flex items-center w-[80%] md:w-[40%] flex-col gap-2">
-        <div className="flex w-full gap-4 items-center justify-start bg-white px-4 py-2 rounded-2xl md:w-80">
-          <GoMail className="bg-white text-2xl" />
-          <input type="text" name="" id="" placeholder="Enter your email address" className="w-full" />
+      <div className="flex flex-col items-center gap-2 w-[80%] md:w-[50%]">
+        <div className="flex justify-start items-center gap-4 bg-white px-4 py-2 rounded-2xl w-full md:w-80">
+          <RiMailLine className="bg-white text-2xl text-gray-500" />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter your email address"
+            className="bg-transparent 2xl:placeholder:pl-6 max-2xl:placeholder:pl-2.5 w-full placeholder:font-normal placeholder:text-sm outline-none"
+          />
         </div>
-        <Button className="bg-white w-full px-4 py-2 rounded-2xl text-black text-center font-lg hover:bg-slate-500 hover:text-white cursor-pointer md:w-80">
+        <Button className="bg-white hover:bg-gray-200 px-4 py-2 rounded-2xl w-full md:w-80 font-lg text-black text-center hover:text-black placeholder:transition-colors cursor-pointer">
           Subscribe to Newsletter
         </Button>
       </div>
