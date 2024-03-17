d39d4576edb48f392e1f6973b7ccef9fb1f0a704
Nicolas <nicolas.esc100@gmail.com>
2024-03-17 15:44:06 -0300

    Diseño del hero-section para mobile completado

--- app/(public)/_components/hero-section.tsx
+++ app/(public)/_components/hero-section.tsx
@@ -10,21 +10,21 @@ const IntegralCF = localFont({
 const Satoshi = localFont({
   src: [
     {
-      path: '../../../public/fonts/Satoshi-Light.woff',
-      weight: '300',
-      style: 'normal',
+      path: "../../../public/fonts/Satoshi-Light.woff",
+      weight: "300",
+      style: "normal",
     },
     {
-      path: '../../../public/fonts/Satoshi-Regular.woff',
-      weight: '400',
-      style: 'normal',
+      path: "../../../public/fonts/Satoshi-Regular.woff",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../../../public/fonts/Satoshi-Bold.woff',
-      weight: '800',
-      style: 'normal',
-    }
-  ]
+      path: "../../../public/fonts/Satoshi-Bold.woff",
+      weight: "800",
+      style: "normal",
+    },
+  ],
 });
 
 const data = [
@@ -35,33 +35,37 @@ const data = [
 
 export const HeroSection = () => {
   return (
-    <section
-      className={`${Satoshi.className} w-full `}>
-      <div className='relative w-[100%] h-[680px]'>
+    <section className={`${Satoshi.className} w-full flex flex-col`}>
+      <div className='relative w-[100%] sm:h-[680px] h-[630px]'>
         <Image
           src='/bg-hero.png'
           alt='bg-hero-image'
           objectFit='cover'
           layout='fill'
+          className='md:flex hidden'
         />
-        <div className='absolute pl-28 text-black h-full flex flex-row'>
-          <div className='flex flex-col justify-center h-full gap-5'>
-            <h2 className={`${IntegralCF.className} font-black text-6xl`}>
-              FIND CLOTHES <br />
-              THAT MATCHES <br />
-              YOUR STYLE
-            </h2>
-            <p className='text-slate-500 text-base font-normal w-[600px]'>
-              Browse through our diverse range of meticulously crafted garments,
-              designed to bring out your individuality and cater to your sense
-              of style.
-            </p>
+        <div className='absolute sm:pl-10 md:pl-28 text-black sm:h-full flex flex-col sm:flex-row'>
+          <div className='flex flex-col justify-center h-full gap-7 sm:gap-5 mt-12 sm:mt-0'>
+            <div className='flex flex-col pl-5 gap-7 sm:gap-5 sm:pl-0'>
+              <h2
+                className={`${IntegralCF.className} font-black text-4xl sm:text-6xl`}>
+                FIND CLOTHES <br />
+                THAT MATCHES <br />
+                YOUR STYLE
+              </h2>
+              <p className='text-slate-500 text-base font-normal sm:w-[600px]'>
+                Browse through our diverse range of meticulously crafted
+                garments, designed to bring out your individuality and cater to
+                your sense of style.
+              </p>
+            </div>
             <Button
               variant='default'
-              className='rounded-[62px] text-base font-light w-52 py-7 bg-black'>
+              className='rounded-[62px] text-base font-light w-11/12 py-7 bg-black mx-auto sm:mx-0 px-10 sm:w-52'>
               Shop now
             </Button>
-            <div className='flex flex-row gap-6 py-10'>
+            {/*   Desktop Data   */}
+            <div className='sm:flex flex-col sm:flex-row gap-6 py-10 hidden'>
               {data.map((item, index) => (
                 <div key={item.description} className='flex gap-5'>
                   <div className='flex flex-col gap-1'>
@@ -83,6 +87,37 @@ export const HeroSection = () => {
                 </div>
               ))}
             </div>
+            {/*   Mobile Data   */}
+            <div className='flex flex-col sm:hidden'>
+              <div className='flex flex-row gap-6 pt-10 justify-center'>
+                {data.slice(0, 2).map((item, index) => (
+                  <div key={item.description} className='flex gap-5'>
+                    <div className='flex flex-col gap-1'>
+                      <p className='text-black font-bold text-3xl'>
+                        {item.count}
+                      </p>
+                      <p className='text-slate-500 font-normal text-base'>
+                        {item.description}
+                      </p>
+                    </div>
+                    <div>
+                      {index === 0 && (
+                        <Separator
+                          orientation='vertical'
+                          className='bg-slate-300'
+                        />
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className='flex flex-col items-center gap-1 pt-10'>
+                <p className='text-black font-bold text-3xl'>{data[2].count}</p>
+                <p className='text-slate-500 font-normal text-base'>
+                  {data[2].description}
+                </p>
+              </div>
+            </div>
           </div>
           <div>
             <Image
@@ -91,7 +126,7 @@ export const HeroSection = () => {
               objectFit='cover'
               width={90}
               height={90}
-              className='absolute left-[38rem] top-12 animate-moveRightLeft'
+              className='hidden lg:flex absolute left-[38rem] top-12'
             />
             <Image
               src='/star-hero.svg'
@@ -99,13 +134,20 @@ export const HeroSection = () => {
               objectFit='cover'
               width={50}
               height={50}
-              className='absolute left-11 top-56 animate-moveRightLeft'
+              className='hidden lg:flex absolute left-11 top-56'
             />
           </div>
         </div>
       </div>
+      <div className='flex justify-center'>
+        <Image
+          src='/bg-hero-mobile.png'
+          alt='bg-hero-image'
+          width={700}
+          height={500}
+          className='md:hidden'
+        />
+      </div>
     </section>
   );
 };
-
-//2xl:max-screen-lg max-w-screen-xl
