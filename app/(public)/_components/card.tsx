73ee1f3d2d1c5c1c66475bc7f014e6589ea119b9
LostoVayne <moises.barrera.devmail.com>
2024-03-18 21:13:40 -0300

    agregados los cambios en los productos

--- app/(public)/_components/card.tsx
+++ app/(public)/_components/card.tsx
@@ -1,54 +1,51 @@
 import { Card } from "@/components/ui/card";
-import { CardProp } from "@/interfaces";
+import { Products } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
 import { RiStarFill, RiStarHalfFill } from "react-icons/ri";
 
-/**
- * Render a card product with the given props.
- *
- * @param {CardProp} props - The props for the card product.
- * @returns {JSX.Element} The rendered card product.
- */
+export const CardProduct = (product: Products): JSX.Element => {
+  const { name, slug, image } = product;
 
-export const CardProduct = ({ slug, name, price, image, rating }: CardProp): JSX.Element => {
   return (
-    <Card>
-      <Link href={`/product/${slug}`}>
-        <div className="flex flex-col justify-start items-start w-[12.375rem] lg:w-[18.44rem] h-[18.1875rem] lg:min-h-[24.83rem]">
+    <Link href={`/product/${slug}`}>
+      <div className="flex flex-col justify-start items-start w-full max-w-[13.5rem]">
+        <div className="bg-[#EAE4DD] rounded-2xl">
           <Image
-            src={image}
+            src={`${image[0]}`}
             alt={name}
-            width={80}
-            height={85}
-            className="mx-auto rounded-2xl w-[198px] lg:w-[295px] h-[200px] lg:h-[298px] object-contain"
+            width={500}
+            height={800}
+            className="w-[14rem] h-[14.5rem] object-contain"
           />
-          <h3 className="mt-3 font-bold text-base text-black text-start lg:text-xl capitalize">
+        </div>
+        <div className="px-2">
+          <h3 className="mt-3 font-bold text-base text-black text-start xl:text-lg capitalize">
             {name}
           </h3>
           <div className="flex justify-start gap-2 lg:gap-4 item-center">
-            <div id="rating" className="flex justify-center items-center text-lg text-yellow-500">
+            <div id="rating" className="flex justify-center items-center text-[#FFC633] text-lg">
               <RiStarFill />
               <RiStarFill />
               <RiStarFill />
               <RiStarFill />
               <RiStarHalfFill />
             </div>
-            <span className="text-base text-slate-300">
-              <i>{rating}/5</i>
+            <span className="font-medium text-gray-500 text-xs">
+              {product.rating}/5
             </span>
           </div>
-          <div className="flex justify-start items-center gap-2 lg:gap-5">
-            <span className="font-medium text-base text-black lg:text-xl">${price}</span>
-            <span className="font-medium text-base text-slate-300 lg:text-xl line-through">
-              ${price + price * 0.2}
+          <div className="flex justify-start items-center gap-2 xl:gap-3 mt-1.5">
+            <span className="font-bold text-base text-black xl:text-lg">${product.price}</span>
+            <span className="font-normal text-gray-300 text-xs xl:text-sm line-through">
+              ${product.price + product.price * 0.2}
             </span>
-            <span className="bg-red-100 px-2 lg:px-3 py-0 lg:py-1 rounded-full font-light text-red-500 text-sm lg:text-pretty">
+            <span className="bg-red-100/90 px-1.5 xl:px-2 py-1.5 rounded-full font-semibold text-red-400 text-xs lg:text-pretty">
               -20%
             </span>
           </div>
         </div>
-      </Link>
-    </Card>
+      </div>
+    </Link>
   );
 };
