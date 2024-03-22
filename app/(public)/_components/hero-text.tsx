1a41cd6f00395a347e6fbb74eb56797094e5cc86
LostoVayne <moises.barrera.devmail.com>
2024-03-21 22:56:27 -0300

    implementadas mejoras

--- app/(public)/_components/hero-text.tsx
+++ app/(public)/_components/hero-text.tsx
@@ -9,48 +9,41 @@ const IntegralCF = localFont({
 
 export const HeroText = () => {
   return (
-    <div className='flex justify-start items-center md:w-[60%] 2xl:w-[50%] h-full'>
-      <div className='flex flex-col items-start gap-7 md:gap-4 lg:gap-6 px-5 md:px-0 md:pl-3'>
+    <div className="flex justify-start items-center md:w-[60%] 2xl:w-[50%] h-full">
+      <div className="flex flex-col items-start gap-7 md:gap-4 lg:gap-6 px-5 md:px-0 md:pl-3">
         <h2
-          className={`${IntegralCF.className} pt-8 md:pt-0 font-black text-4xl sm:text-5xl md:text-4xl lg:text-5xl 2xl:text-6xl`}>
-          FIND CLOTHES <br />
-          THAT MATCHES <br />
-          YOUR STYLE
+          className={`${IntegralCF.className} text-balance pt-8 md:pt-0 font-black text-4xl md:text-4xl lg:text-5xl 2xl:text-6xl`}
+        >
+          FIND CLOTHES THAT MATCHES YOUR STYLE
         </h2>
-        <p className='py-3 font-normal text-slate-500 sm:text-sm 2xl:text-base'>
-          Browse through our diverse range of meticulously crafted garments,
-          designed to bring out your individuality and cater to your sense of
-          style.
+        <p className="py-3 font-normal text-balance text-slate-500 sm:text-sm 2xl:text-base">
+          Browse through our diverse range of meticulously crafted garments, designed to bring out your
+          individuality and cater to your sense of style.
         </p>
         <Button
-          variant='default'
-          className='bg-black hover:bg-black/90 py-8 rounded-[62px] min-w-full sm:min-w-96 md:min-w-60 lg:h-14 font-light text-base'>
+          variant="default"
+          className="bg-black hover:bg-black/90 py-8 rounded-[62px] min-w-full sm:min-w-96 md:min-w-60 lg:h-14 font-light text-base"
+        >
           Shop now
         </Button>
-        <div className='flex sm:flex-row flex-col justify-center items-center gap-4 m-auto sm:m-0 pt-3 md:pt-0'>
-          <div className='flex flex-row items-center gap-4'>
+        <div className="flex sm:flex-row flex-col justify-center items-center gap-4 m-auto sm:m-0 pt-3 md:pt-0">
+          <div className="flex flex-row items-center gap-4">
             {HeroData.slice(0, 2).map((item, index) => (
-              <div key={index} className='flex gap-5'>
-                <div className='flex flex-col gap-1'>
-                  <p className='font-bold text-2xl text-black 2xl:text-4xl'>
-                    {item.count}
-                  </p>
-                  <p className='font-normal text-slate-500 text-sm 2xl:text-base'>
-                    {item.description}
-                  </p>
+              <div key={index} className="flex gap-5">
+                <div className="flex flex-col gap-1">
+                  <p className="font-bold text-2xl text-black 2xl:text-4xl">{item.count}</p>
+                  <p className="font-normal text-slate-500 text-sm 2xl:text-base">{item.description}</p>
                 </div>
                 <div>
-                  <Separator orientation='vertical' className='bg-slate-300' />
+                  <Separator orientation="vertical" className="bg-slate-300" />
                 </div>
               </div>
             ))}
           </div>
-          <div className='flex gap-4'>
-            <div className='flex flex-col gap-1'>
-              <p className='font-bold text-2xl text-black 2xl:text-4xl'>
-                {HeroData[2].count}
-              </p>
-              <p className='font-normal text-slate-500 text-sm 2xl:text-base'>
+          <div className="flex gap-4">
+            <div className="flex flex-col gap-1">
+              <p className="font-bold text-2xl text-black 2xl:text-4xl">{HeroData[2].count}</p>
+              <p className="font-normal text-slate-500 text-sm 2xl:text-base">
                 {HeroData[2].description}
               </p>
             </div>
